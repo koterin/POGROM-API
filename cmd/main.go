90e06e5cc2f1621c1e3645a81ccf41f211d44446
koterin <katerine.grin@gmail.com>
2022-08-17 22:42:51 +0300

    removed sigint handling for now

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-    "os"
-    "os/signal"
-    "syscall"
-
     log "github.com/sirupsen/logrus"
     "pogrom/config"
     "pogrom/internal/app"
 )
 
 func main() {
-    c := make(chan os.Signal, 1)
-    signal.Notify(c, os.Interrupt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGQUIT)
-
     config.Validate()
 
     level, err := log.ParseLevel(config.Args.LOG_LEVEL)
@@ -25,7 +18,4 @@ func main() {
     log.SetLevel(level)
 
     app.Run()
-
-    <-c
-    log.Println("Server gracefully shut down")
 }
